bbfa30bc2b8674150ea75d41f0c55643f9ccbcc4
shiva shankar <shankuu786@gmail.com>
2023-07-04 19:28:30 +0000

    get error

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ const initializeDBAndServer = async () => {
       filename: databasePath,
       driver: sqlite3.Database,
     });
-    app.listen(3001, () => {
-      console.log("Server Running at http://localhost:3001/");
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
     });
   } catch (e) {
     console.log(`DB Error: ${e.message}`);
@@ -72,3 +72,64 @@ app.get("/movies/:movieId/", async (request, response) => {
   const movieDetails = await database.get(getMovieQuery);
   response.send(convertDBObjectToResponseObject(movieDetails));
 });
+
+// update movie table
+app.put("/movies/:movieId/", async (request, response) => {
+  const { directorId, movieName, leadActor } = request.body;
+  const { movieId } = request.params;
+  const updateMovieDetailsQuery = `
+    UPDATE
+    movie
+    SET
+    director_id = ${directorId},
+    movie_name = '${movieName}',
+    lead_actor = '${leadActor}'
+    WHERE
+    movie_id = ${movieId};`;
+  await database.run(updateMovieDetailsQuery);
+  response.send("Movie Details Updated");
+});
+
+//delete movie
+app.delete("/movies/:movieId/", async (request, response) => {
+  const { movieId } = request.params;
+  const deleteQuery = `
+    DELETE
+    FROM
+    movie
+    WHERE
+    movie_id = ${movieId};`;
+  await database.run(deleteQuery);
+  response.send("Movie Removed");
+});
+
+//get all directors
+app.get("/directors/", async (request, response) => {
+  const getAllDirectorsQuery = `
+    SELECT
+    *
+    FROM
+    director;`;
+  const allDirectors = await database.all(getAllDirectorsQuery);
+  response.send(convertDBObjectToResponseObject(allDirectors));
+});
+
+//get director movies
+app.get("/directors/:directorId/movies/", async (request, response) => {
+  const { directorId } = request.params;
+  const getDirectorMoviesQuery = `
+    SELECT
+    *
+    FROM
+    movie
+    WHERE
+    director_id = ${directorId};`;
+  directorMovies = await database.get(getDirectorMoviesQuery);
+  response.send(
+    directorMovies.map((eachMovie) => ({
+      directorId: eachMovie.director_id,
+      directorName: eachMovie.directorName,
+    }))
+  );
+});
+module.exports = app;
